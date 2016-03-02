9d64495946656ce1f75909c7b928d55dbdd8e573
Adán G. Galván González <bialikoer@gmail.com>
2016-03-01 21:01:18 -0500

    get info from api

--- src/client/app/admin/admin.controller.js
+++ src/client/app/admin/admin.controller.js
@@ -10,23 +10,18 @@
     function AdminController(logger, dataservice) {
         var vm = this;
         vm.title = 'Admin';
-        vm.hashtag="#Oscars #madmax";
-        vm.data = [
-            { y: "2006", a: 100, b: 90 },
-            { y: "2007", a: 75,  b: 65 },
-            { y: "2008", a: 50,  b: 40 },
-            { y: "2009", a: 75,  b: 65 },
-            { y: "2010", a: 50,  b: 40 },
-            { y: "2011", a: 75,  b: 65 },
-            { y: "2012", a: 100, b: 90 }
-        ];
-        vm.xkey = 'y';
-        vm.ykeys = ["a", "b"];
-        vm.labels = ["Positive Comments", "Negative Comments"];
-        vm.colors = ["#31C0BE", "#c7254e"];
-        dataservice.getSentiment().
+        
+        function activate(data){
+            vm.data = data;
+            vm.hashtag="#Oscars #madmax";
+            vm.xkey = 'y';
+            vm.ykeys = ["a", "b"];
+            vm.labels = ["Positive Comments", "Negative Comments"];
+            vm.colors = ["#31C0BE", "#c7254e"];
+        }
+        dataservice.getHistory().
         then(function (data){
-            vm.history = data;
+            activate(data);
         })
         .catch(function(err){
             console.log(err);
--- src/client/app/core/dataservice.js
+++ src/client/app/core/dataservice.js
@@ -11,7 +11,7 @@
         var service = {
             getPeople: getPeople,
             getMessageCount: getMessageCount,
-            getSentiment: getSentiment
+            getHistory: getHistory
         };
 
         return service;
@@ -32,17 +32,37 @@
             }
         }
         
-        function getSentiment(){
+        function getHistory(){
             return $http.get('/api/words/history')
                 .then(success)
                 .catch(fail);
 
             function success(response) {
-                return response.data;
+                console.log(response.data);
+                
+                //var data = _.map(response.data, createChartValues);
+                var data = [
+                    { y: "2006", a: 100, b: 90 },
+                    { y: "2007", a: 75,  b: 65 },
+                    { y: "2008", a: 50,  b: 40 },
+                    { y: "2009", a: 75,  b: 65 },
+                    { y: "2010", a: 50,  b: 40 },
+                    { y: "2011", a: 75,  b: 65 },
+                    { y: "2012", a: 100, b: 90 }
+                ];
+                return data;
             }
 
             function fail(e) {
                 return exception.catcher('XHR Failed for getPeople')(e);
+            }
+            
+            function createChartValues(item){
+                return { 
+                    y: moment(item.created_on).format('h:mm:ss'),
+                    a:  (item.score > 0)? item.score : 0,
+                    b: (item.score < 0)? item.score : 0
+                };
             }
         }
     }
